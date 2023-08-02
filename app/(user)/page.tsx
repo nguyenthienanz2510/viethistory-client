4ff9f4e8a747dae390a78ba629dcb8bf1a717985
nguyenthienan <nguyenthienanz2510@gmail.com>
2023-08-03 02:26:44 +0700

    +add: setup source

---
+++ app/(user)/page.tsx
@@ -0,0 +1,15 @@
+
+
+export default function Home() {
+  return (
+    <main>
+      <h1 className="text-color-text-primary">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt laudantium quis voluptas reprehenderit? Totam maiores commodi eveniet ipsam non animi!</h1>
+      <h1 className="text-color-text-default">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt laudantium quis voluptas reprehenderit? Totam maiores commodi eveniet ipsam non animi!</h1>
+      <h1 className="text-slate-100">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt laudantium quis voluptas reprehenderit? Totam maiores commodi eveniet ipsam non animi!</h1>
+      <h1 className="text-slate-300">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt laudantium quis voluptas reprehenderit? Totam maiores commodi eveniet ipsam non animi!</h1>
+      <h1 className="text-slate-400">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt laudantium quis voluptas reprehenderit? Totam maiores commodi eveniet ipsam non animi!</h1>
+      <h1 className="text-slate-500">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt laudantium quis voluptas reprehenderit? Totam maiores commodi eveniet ipsam non animi!</h1>
+      <h1 className="text-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt laudantium quis voluptas reprehenderit? Totam maiores commodi eveniet ipsam non animi!</h1>
+    </main>
+  )
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,61 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+    container: {
+      center: true,
+      screens: {
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1356px'
       },
+      padding: {
+        DEFAULT: '12px',
+        sm: '0'
+      }
     },
-  },
-  plugins: [],
+    extend: {
+      colors: {
+        'color-primary': '#38BDF8',
+        'color-secondary': '#EC4899',
+        'color-third': '#3B82F6',
+
+        'color-text-default': '#94A3B8',
+        'color-text-primary': '#E2E8F0',
+        'color-bg-default': '#10172a',
+        'color-bg-hover': '#161f32',
+
+        'color-white': '#ffffff',
+        'color-black': '#000000',
+        'color-success': '#03a66d',
+        'color-info': '#17a2b8',
+        'color-warning': '#ffc107',
+        'color-danger': '#cf304a'
+      },
+      fontSize: {
+        10: '10px',
+        12: '12px',
+        14: '14px',
+        16: '16px',
+        18: '18px',
+        20: '20px',
+        22: '22px',
+        24: '24px',
+        26: '26px',
+        28: '28px',
+        30: '30px',
+        32: '32px',
+        34: '34px',
+        36: '36px',
+        38: '38px',
+        40: '40px'
+      }
+    }
+  }
 }
